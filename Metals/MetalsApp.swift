53577cf1a5547ad93a72137b3fc9655efa043f56
Can Yoldas <canyoldas1997@gmail.com>
2024-05-27 21:03:13 +0200

    Initial Commit

---
+++ Metals/MetalsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetalsApp.swift
+//  Metals
+//
+//  Created by Can Yoldas on 27/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct MetalsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
